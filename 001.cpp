9b5bc66a07a5d570331a84d06b8e09f139215894
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-12-11 16:11:45 +0300

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -1,108 +1,116 @@
-#include <iostream>
-#include <string>
+#include "stdafx.h"
+#include <sstream>
+#include<iostream>
+#include <utility>
+
 using namespace std;
 
-int count = 0;
-struct node_t {
-    int value;
-    node_t * next;
+struct node_t 
+{
+	int value;
+	node_t * next;
 };
 
-void add(node_t * & head, int value) {
-    node_t * node = new node_t;
-    node->value = value;
-    node->next = nullptr;
-    if( head == nullptr) {
-        head = node;
-    }
-    else {
-        node_t * last = head;
-        while(last->next) {
-            last = last->next;
-        }
-        last->next = node;
-    }
-    count++;
-}
+bool read(char & op, int & value) {
+	string line;
+	getline(cin, line);
+	istringstream stream(line);
 
-void away(node_t * & list) {
-    node_t * first = list;
-    list = list->next;
-    delete first;
-    count--;
+	if (stream >> op) {
+		if (op == '+' && stream >> value) {
+			return true;
+		}
+		else if (op == '-' || op == '/' || op == '=' || op == 'q') {
+			return true;
+		}
+	}
+	return false;
 }
+void add_to_end(node_t * & head , int value)
+{
+	node_t * node = new node_t;
+	node->value = value;
+	node->next = nullptr;
 
-void reverse(node_t * list) {
-    node_t * begin = list;
-    node_t * last = begin;
-    for (int j = 0; j < count / 2 ; j++) {
-        for (int i = j; i < count - 1 - j; i++) {
-            last = last->next;
-        }
-        int saved = begin->value;
-        begin->value = last->value;
-        last->value = saved;
-        begin = begin->next;
-        last = begin;
-    }
+	if (head == nullptr) {
+		head = node;
+	}
+	else {
+		node_t * last = head; 
+		while (last->next) {
+			last = last->next;
+		}
+		last->next = node;
+	}
 }
+void delete_first(node_t * & head)
+{
+	node_t *time = head; // указатель 
+	head = head->next;
+	delete time; // т.к указатель , то при удалении удалиться и сам "элемент"
+}
+void print(node_t * head) {
+	node_t *p;
+	p = head;
+	while (p) {
+		cout << "+---+" << "    ";
+		p = p->next;
+	}
+	p = head;
+	cout << endl;
 
-void print(node_t * first, int count) {
-    node_t *print = first;
-    for (int i = 0; i < count; i++) {
-        cout << "+---+    ";
-    }
-    cout << endl;
-    for (int i = 0; i < count; i++) {
-        if (i != 0) {
-            cout << "--->";
-        }
-        cout << "| " << print->value << " |";
-        print = print->next;
-    }
-    cout << endl;
-    for (int i = 0; i < count; i++) {
-        cout << "+---+    ";
-    }
-    cout << endl;
+	while (p) {
+		cout << "| " << p->value << " |";
+		p = p->next;
+		if (p) {
+			cout << "--->";
+		}
+	}
+	p = head;
+	cout << endl;
+
+	while (p) {
+		cout << "+---+" << "    ";
+		p = p->next;
+	}
+	cout << endl;
 }
-void delet(node_t * head) {
-    while(head) {
-        delete head;
-        head = head->next;
-    }
+void reverse(node_t * &head)
+{
+	node_t*new_head = nullptr;
+	
+	for (node_t*node = head; node; )
+	{
+		node_t * node_next = node->next;
+		node->next = new_head;
+		new_head = node;
+		node = node_next;
+	}
+	head = new_head;
 }
 int main()
 {
-    int value;
-    char op;
-    node_t * head = nullptr;
-    while(cin >> op) {
-        switch (op) {
-            case '+' : {
-                cin >> value;
-                add(head, value);
-                print(head, count);
-                break;
-            };
-            case '-' : {
-                away(head);
-                print(head, count);
-                break;
-            };
-            case 'q' : {
-                exit(0);
-            };
-            case '=' : {
-                print(head, count);
-                break;
-            };
-            case '/' : {
-                reverse(head);
-                print(head, count);
-                break;
-            };
-        }
-    }
-    delet(head);
+	node_t * head = nullptr;
+	char op ;
+	int value;
+	while (read(op,value)) {
+		if (op == 'q') {
+			break;
+		}
+		switch (op) {
+		case '+':
+			add_to_end(head, value);
+			break;
+		case '-':
+			delete_first(head);
+			break;
+		case '=':
+			print(head);
+			break;
+		case'/':
+			reverse(head);
+			break;
+		}
+	}
+	return 0;
 }
